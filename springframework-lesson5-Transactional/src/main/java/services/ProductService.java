4d46ecf50ea8d10d5ad5e1fc3824036f99e2833a
rob3rt96 <robert.popescu195@gmail.com>
2023-05-02 22:32:40 +0300

    Add update to the @Transactional wrapped method

--- springframework-lesson5-Transactional/src/main/java/services/ProductService.java
+++ springframework-lesson5-Transactional/src/main/java/services/ProductService.java
@@ -12,8 +12,8 @@ public class ProductService {
     private ProductRepository productRepository;
 
     @Transactional                                      // This annotation makes possible to wrap this method in a transaction. The PlatformTransactionManager will manage the execution of this method.
-    public void addOneProduct() {                       // If an operation in the method throws an Exception, the PlatformTransactionManager will perform a roll-back to undo all the processes before the Exception that happened.
-        productRepository.addProduct("Apple");
+    public void addOneProduct() {                       // If an operation in the method throws an RuntimeException, the PlatformTransactionManager will perform a rollback to undo all the processes before the Exception that happened.
+        productRepository.addProduct("Apple");      // It will not, by default, rollback a checked Exception.
 //        throw new RuntimeException(":(");
     }
 }
