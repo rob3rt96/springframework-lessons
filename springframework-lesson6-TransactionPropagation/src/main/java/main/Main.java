7d8761c02d2615332b449be1ee44b718edc4e70d
rob3rt96 <robert.popescu195@gmail.com>
2023-05-03 12:13:09 +0300

    Add initial commit for springframework-lesson6

---
+++ springframework-lesson6-TransactionPropagation/src/main/java/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (var context = new AnnotationConfigApplicationContext()) {
+
+        }
+    }
+}
