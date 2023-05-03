bde903d0a73618a12ec0c5062210e06500d5bfb2
rob3rt96 <robert.popescu195@gmail.com>
2023-05-03 21:33:40 +0300

    Add configuration of @Service and @Repository beans

---
+++ springframework-lesson7-Transaction_Isolation/src/main/java/services/ProductService.java
@@ -0,0 +1,24 @@
+package services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Transactional
+    public void addTenProducts() {
+        for (int i = 0; i < 10; i++) {
+            productRepository.addProduct("Product " + i);
+        }
+    }
+}
