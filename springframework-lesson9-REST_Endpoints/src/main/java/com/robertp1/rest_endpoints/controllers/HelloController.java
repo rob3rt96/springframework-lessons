14a457ae5953c2bf4fb887a3195b58dbfc38a137
rob3rt96 <robert.popescu195@gmail.com>
2023-05-06 21:31:35 +0300

    Add implementation of two ways to retrieve information clients to your endpoints

--- springframework-lesson9-REST_Endpoints/src/main/java/com/robertp1/rest_endpoints/controllers/HelloController.java
+++ springframework-lesson9-REST_Endpoints/src/main/java/com/robertp1/rest_endpoints/controllers/HelloController.java
@@ -1,5 +1,6 @@
 package com.robertp1.rest_endpoints.controllers;
 
+import com.robertp1.rest_endpoints.dto.Person;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,5 +18,10 @@ public class HelloController {
     @GetMapping(path = "/hello/{name}")
     public String hello(@PathVariable("name") String name) {    // The @PathVariable's parameter, "name", is not mandatory if the method's parameter name is the same as the path variable declared in the @RequestMapping's path parameter.
         return "Hello there, " + name + "!";
+    }
+
+    @PostMapping(path = "/goodbye")
+    public String goodbye(@RequestBody Person p) {      // The Person object will be taken from the body of the request. The response will be converted from JSON to a java Object.
+        return "Goodbye, " + p.getName() + "!";
     }
 }
---
+++ springframework-lesson9-REST_Endpoints/src/main/java/com/robertp1/rest_endpoints/dto/Person.java
@@ -0,0 +1,18 @@
+package com.robertp1.rest_endpoints.dto;
+
+/**
+ * DTO means Data Transfer Object
+ */
+
+public class Person {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
