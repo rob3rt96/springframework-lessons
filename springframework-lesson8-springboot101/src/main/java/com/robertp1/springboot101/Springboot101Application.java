37f66f8cfa8622d49ad370e38cfa3c81d8a3a607
rob3rt96 <robert.popescu195@gmail.com>
2023-05-07 09:57:50 +0300

    Add commit lesson 8

---
+++ springframework-lesson8-springboot101/src/main/java/com/robertp1/springboot101/Springboot101Application.java
@@ -0,0 +1,13 @@
+package com.robertp1.springboot101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot101Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot101Application.class, args);        // This is where the application starts, and where the Context is created.
+    }
+
+}
---
+++ springframework-lesson9-REST_Endpoints/src/main/java/com/robertp1/rest_endpoints/SpringbootRestEndpointsApplication.java
@@ -0,0 +1,13 @@
+package com.robertp1.rest_endpoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestEndpointsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRestEndpointsApplication.class, args);
+    }
+
+}
