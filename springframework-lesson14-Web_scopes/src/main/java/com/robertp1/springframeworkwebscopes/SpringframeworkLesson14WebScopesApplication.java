9d1d82db64bde8febe86788cb3b6f732fd336dc5
rob3rt96 <robert.popescu195@gmail.com>
2023-05-11 16:37:22 +0300

    Add initial commit of lesson 14

---
+++ springframework-lesson14-Web_scopes/src/main/java/com/robertp1/springframeworkwebscopes/SpringframeworkLesson14WebScopesApplication.java
@@ -0,0 +1,13 @@
+package com.robertp1.springframeworkwebscopes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringframeworkLesson14WebScopesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringframeworkLesson14WebScopesApplication.class, args);
+    }
+
+}
