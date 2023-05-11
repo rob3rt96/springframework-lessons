9d34ae1fc8849e7ca2da17b20fb34eb7eb6d9827
rob3rt96 <robert.popescu195@gmail.com>
2023-05-11 09:58:49 +0300

    Add the logic for creating beans with PasswordEncoder, SecurityFilterChain and UserDetailsService

--- springframework-lesson13-Contract_in_SpringSecurity/src/main/java/com/robertp1/springbootspringsecurity/config/ProjectConfig.java
+++ springframework-lesson13-Contract_in_SpringSecurity/src/main/java/com/robertp1/springbootspringsecurity/config/ProjectConfig.java
@@ -2,8 +2,15 @@ package com.robertp1.springbootspringsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
@@ -15,9 +22,45 @@ public class ProjectConfig {
      */
 
     @Bean
+    public PasswordEncoder passwordEncoder() {
+        // return new BCryptPasswordEncoder();      /* In real life we would use this PasswordEncoder implementation, because NoOpPasswordEncoder is only for testing and learning purposes.*/
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    // This provides to Springboot the possibility to find and access the users.
+    @Bean
     public UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
 
+        UserDetails user1 = User.withUsername("dwightSchrute")
+                .password("123456")
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user2 = User.withUsername("jimHalpert")
+                .password("123456")
+                .roles("MANAGER")
+                .build();
+
+        manager.createUser(user1);
+        manager.createUser(user2);
+
         return manager;
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.httpBasic();
+
+//        http.authorizeHttpRequests()
+//                .requestMatchers(HttpMethod.GET).hasRole("ROLE_ADMIN")
+//                .requestMatchers(HttpMethod.POST).hasAnyRole("ROLE_MANAGER")
+//                .anyRequest().permitAll();
+
+        http.authorizeHttpRequests()
+                .requestMatchers("/hello").hasRole("ADMIN")
+                .anyRequest().permitAll();
+
+        return http.build();
+   }
 }
